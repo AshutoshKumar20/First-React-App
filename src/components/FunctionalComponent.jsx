7cd091087239e9004166d265fb50daa9eda6fe8a
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2023-01-27 20:37:49 +0530

    First Day of React.js Completed

---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FunctionalComponent() {
+    return (
+        <div>
+            <p>This is a Functional Component</p>
+        </div>
+    );
+};
+
+export default FunctionalComponent;
