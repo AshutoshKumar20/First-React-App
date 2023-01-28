e4a989847937b211ff0e345a30d1c0dc7842fd9a
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2023-01-28 16:59:50 +0530

    Practicing with States and Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import ClassComponent from './components/ClassComponent';
 import FunctionalComponent from './components/FunctionalComponent';
 
 function App() {
+  const company = "MicroSoft";
+
+  const [name, setName] = useState("Ashutosh")
+
   return (
     <div className="App">
       <h1>Class Component</h1>
       <ClassComponent />
       <br />
       <h1>Functional Component</h1>
-      <FunctionalComponent />
+      <FunctionalComponent
+        name={name}
+        age={19}
+        company={company}
+        setName={setName} />
     </div>
   );
 }
--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function FunctionalComponent() {
+const FunctionalComponent = ({ name, age, company, setName }) => {
+    const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("")
+
+    const subtractConst = () => {
+        setCount(count - 1);
+    };
+
+    // const { name, age, company, setName } = props;
+
     return (
         <div>
             <p>This is a Functional Component</p>
+            <button onClick={() => setCount(count + 1)}>Click me to Add 1</button>
+            <button onClick={subtractConst}>Click me to Subtract 1</button>
+            <h1>{count}</h1>
+            <h1>My name is {name}. I am {age}, I work for {company}.
+            </h1>
+            <input onChange={(e) => setChangeName(e.target.value)} />
+            <button onClick={() => setName(changeName)}>Change Name</button>
         </div>
     );
 };
